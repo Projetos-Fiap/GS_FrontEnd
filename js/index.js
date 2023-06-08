be4bc309c9ff67c57e58295d35e7710cf9b9cea8
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-06-07 22:09:36 -0300

    colocando o carrossel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 alert('Bem Vindo!')
+
+document.addEventListener('DOMContentLoaded', function() {
+    var carouselImages = document.querySelectorAll('.carousel-image');
+    var carouselButtons = document.querySelectorAll('.carousel-button');
+    var currentImageIndex = 0;
+  
+    function showImage(index) {
+      carouselImages.forEach(function(image) {
+        image.classList.remove('active');
+      });
+  
+      carouselImages[index].classList.add('active');
+    }
+  
+    function previousImage() {
+      currentImageIndex--;
+  
+      if (currentImageIndex < 0) {
+        currentImageIndex = carouselImages.length - 1;
+      }
+  
+      showImage(currentImageIndex);
+    }
+  
+    function nextImage() {
+      currentImageIndex++;
+  
+      if (currentImageIndex >= carouselImages.length) {
+        currentImageIndex = 0;
+      }
+  
+      showImage(currentImageIndex);
+    }
+  
+    carouselButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        if (button.classList.contains('previous')) {
+          previousImage();
+        } else if (button.classList.contains('next')) {
+          nextImage();
+        }
+      });
+    });
+  });
+  
